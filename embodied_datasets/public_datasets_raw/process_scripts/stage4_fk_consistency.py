8feba8650738a7aa07530c8fad1b70b0e2f13f78
yuefan.hao <yuefan.hao@geekplus.com>
2026-07-23 12:43:02 +0800

    refactor: move FkChain to shared/fk_backend.py

--- embodied_datasets/public_datasets_raw/process_scripts/stage4_fk_consistency.py
+++ embodied_datasets/public_datasets_raw/process_scripts/stage4_fk_consistency.py
@@ -12,7 +12,7 @@ from dataclasses import replace
 import numpy as np
 
 from shared.episode import Episode, StageResult
-from common.fk_backend import FkChain
+from shared.fk_backend import FkChain
 from common.schema import ProcessConfig
 
 EEF_POS_WIDTH = 3
